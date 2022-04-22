30c184ee4b9c74765e63461b49030db54c731498
Deivid Medina <deivid.medina@mail.escuelaing.edu.co>
2022-04-21 21:08:10 -0500

    Agregando url

--- src/main/resources/RecursosApi/js/servicio.js
+++ src/main/resources/RecursosApi/js/servicio.js
@@ -1,27 +1,26 @@
 var Servicios = (function () {
     function publicarComentario(idtextusuarios,idtextcomentario){
         if(idtextusuarios.length !== 0 && idtextcomentario.length !== 0){
-            const comment={user:idtextusuarios,body:idtextcomentario} //Creando Formato JSon, El nombre tiene que ser igual que en el Usuario
-            console.log(comment);
-            axios.post("/publicarComentario",comment).then(res=>{ //Llamar la instancia Login para validar datos
-                if(res.data==="No se pudo publicar el comentario") {
-                    window.location.href = 'http://localhost:35000/RecursosApi/ServicioPost.html';
-                }
+            const url = 'http://localhost:34000/postT?'+'user='+idtextusuarios+'&body='+idtextcomentario
+            axios.get(url).then(res=>{ //Llamar la instancia Login para validar datos
             })
+            .catch(error=>console.log(error))
         }
         else {
             alert("Antes de Publicar un comentario debe llenar los campos.");
         }
     }
     function getComentarios(){
-        axios.get("/obtnerComentarios").then(res=>{
+        const url = 'http://localhost:7654/Feed'
+        axios.get(url)
+        .then(res=>{
             var i ;
             for (i = 0; i < res.data.length; i ++){
                 var comentarioTitulo = "idComent"+i;
                 document.getElementById(comentarioTitulo).innerHTML = JSON.stringify(res.data[i], null, ' ');
             }
-            //document.getElementById("idComentarios").innerHTML = JSON.stringify(res.data, null, ' ');
         })
+        .catch(error=>console.log(error))
     }
     return {
         publicarComentario:publicarComentario,
